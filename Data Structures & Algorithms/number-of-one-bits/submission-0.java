9e6dfaf787e26dbd6933300c09f9514ee14073a4
Hanith Ganduri <hanithganduri2301@gmail.com>
2026-07-09 19:55:32 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingWeight(int n) {
+        char ch[]=(Integer.toBinaryString(n)).toCharArray();
+        int count=0;
+
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='1'){
+                count++;
+            }
+
+        }
+        return count;
+        
+    }
+}
